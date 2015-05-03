d85e4e4462135ef611d4d3c4bdeeec205d9ea635
metalcrash <wh854@nyu.edu>
2015-05-03 17:22:42 -0400

    updating positional querier

--- query/pquery.py
+++ query/pquery.py
@@ -1,5 +1,6 @@
 import config
 import time
+import cProfile
 
 class PQuerier:
 
@@ -14,6 +15,7 @@ class PQuerier:
 		poss=[]
 		frags=[]
 		fragsdict={}
+		#f1=time.time()
 		for i in xrange(len(docid)):
 			curfrags=self.dvf[docid[i]][versid[i]]
 			frags.append(curfrags)
@@ -21,6 +23,7 @@ class PQuerier:
 				fragsdict[f]=i
 		poss=self.locate(fragsdict,frags,lists,self.fsize)
 		return poss
+		
 	def loadfsize(self):
 		fsizef=open(config.P_INDEX_FSIZE_PATH,"r")
 		records=fsizef.readline()
@@ -51,14 +54,9 @@ class PQuerier:
 		lists=[]
 		for t in terms:
 			meta=lexicon[t]
-			f1=time.time()
 			indexf.seek(meta[0])
 			a=indexf.read(meta[1])
 			b=a.split()
-			#print b[:10]
-			print "len b:"+str(len(b))
-			#tlist=[[b[2*i],b[2*i+1]] for i in xrange(len(b)/2)]
-			#for i in xrange(len(b)):
 			lists.append(b)
 		return lists
 	
@@ -68,19 +66,22 @@ class PQuerier:
 		for i in xrange(len(frags)):
 			poslists.append([])
 			for j in xrange(len(lists)):
-				poslists[i].append([])
+				poslists[i].append({})
 		for i in xrange(len(lists)):
 			valid=False
 			curlist=lists[i]
 			prevfid=0
-			print curlist[:50]
+			lis=0
 			for j in xrange(len(curlist)/2):
 				fid=int(curlist[2*j])+prevfid
 				if fid in fragsdict:
+					lis+=1
 					valid=True
-					poslists[fragsdict[fid]][i].append([fid,int(curlist[2*j+1])])
+					if fid in poslists[fragsdict[fid]][i]:
+						poslists[fragsdict[fid]][i][fid].append(int(curlist[2*j+1]))
+					else:	
+						poslists[fragsdict[fid]][i][fid]=[int(curlist[2*j+1])]
 				prevfid=fid
-			print valid
 			if not valid:return False
 		outposlists=[]
 		for i in xrange(len(frags)):
@@ -89,14 +90,11 @@ class PQuerier:
 				outposlists[i].append([])
 		for i in xrange(len(frags)):
 			pos=0
-			cursor=[]
-			for j in xrange(len(lists)):
-				cursor.append(0)
 			for fid in frags[i]:
 				for j in xrange(len(lists)):
-					if cursor[j]<len(poslists[i][j]) and fid==poslists[i][j][cursor[j]][0]:
-						outposlists[i][j].append(poslists[i][j][cursor[j]][1]+pos)
-						cursor[j]+=1
+					if fid in poslists[i][j]:
+						for x in poslists[i][j][fid]:
+							outposlists[i][j].append(x+pos)
 				pos+=fsize[fid]
 		return outposlists
 	
@@ -104,7 +102,7 @@ if __name__=='__main__':
 	#a testing example: terms=terms,docid=0,vid=0
 	pq=PQuerier()
 	start=time.time()
-	ide=pq.getPositions([1299,1320,432],[11,2,21],['actress','best'])
+	ide=pq.getPositions([1632,1519,375,1295],[22,11,29,25],['international','school'])
 	end=time.time()
 	print ide
 	print end-start
